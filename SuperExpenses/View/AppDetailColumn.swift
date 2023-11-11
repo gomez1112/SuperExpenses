b83c2b257676265d1933da8890529bba15da1a72
Gerard Gomez <gerardgomez11@me.comgerardgomez11@me.comgerardgomez11@me.com>
2023-11-11 11:53:44 -0500

    - Refactor code - Fix bugs - Added navigation to switch between tab or sidebar depending on whether is it is a phone, tv or ipad -

---
+++ SuperExpenses/View/AppDetailColumn.swift
@@ -0,0 +1,18 @@
+//
+//  AppDetailColumn.swift
+//  SuperExpenses
+//
+//  Created by Gerard Gomez on 11/11/23.
+//
+
+import SwiftUI
+
+struct AppDetailColumn: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppDetailColumn()
+}
