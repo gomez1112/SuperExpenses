cbf94f08bcb38abb2d45154dbb3f5233f4e80d36
Gerard Gomez <gerardgomez11@me.comgerardgomez11@me.comgerardgomez11@me.com>
2023-11-11 07:55:51 -0500

    Initial Commit

---
+++ SuperExpenses/SuperExpensesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperExpensesApp.swift
+//  SuperExpenses
+//
+//  Created by Gerard Gomez on 11/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SuperExpensesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
