0f5cff6fbd5ae0943c3e47f24020d40a72f3488c
Gerard Gomez <gerardgomez11@me.comgerardgomez11@me.comgerardgomez11@me.com>
2023-11-12 15:57:36 -0500

    -add widget extension -simplify navigation

---
+++ SuperExpenseWidget/SuperExpenseWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  SuperExpenseWidgetBundle.swift
+//  SuperExpenseWidget
+//
+//  Created by Gerard Gomez on 11/12/23.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct SuperExpenseWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        SuperExpenseWidget()
+    }
+}
