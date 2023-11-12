3c58ce9aec70e108a0aff74c63f5687fdc94d6c6
Gerard Gomez <gerardgomez11@me.comgerardgomez11@me.comgerardgomez11@me.com>
2023-11-12 18:39:28 -0500

    -Refactor code into viewmodels

---
+++ SuperExpenses/Extension/TransactionEditor+Extension.swift
@@ -0,0 +1,8 @@
+//
+//  TransactionEditor+Extension.swift
+//  SuperExpenses
+//
+//  Created by Gerard Gomez on 11/12/23.
+//
+
+import Foundation
---
+++ SuperExpenses/ViewModel/DataModel.swift
@@ -0,0 +1,8 @@
+//
+//  DataModel.swift
+//  SuperExpenses
+//
+//  Created by Gerard Gomez on 11/12/23.
+//
+
+import Foundation
